53bb819aab9cf577538bd27129f66f2f8d545eea
Rafael Aguiar <rlho27o@gmail.com>
2014-10-01 15:30:19 -0700

    adding survival cast

--- tutorials/lab2/Tutorial-2-scripts(1).R
+++ tutorials/lab2/Tutorial-2-scripts(1).R
@@ -4,6 +4,7 @@
 library(SnowballC)
 library(tm)
 library(rpart)
+library(survival)
 setwd("~/Workspace/cpsc_340/tutorials/lab2")
 
 train<- read.table("./epinions/epinions-train.csv", sep = ",", header = TRUE)
@@ -13,8 +14,8 @@ train_set <- train[sample(nrow(train), 100),]
 test_set <- test[sample(nrow(test), 50),]
 
 train_test_set <- rbind(train_set,test_set) 
-train_test_set$class
-train_test_set$text
+# train_test_set$class
+# train_test_set$text
 
 train_test_corpus <- Corpus(VectorSource(train_test_set$text))
 
@@ -22,9 +23,9 @@ train_test_corpus <- Corpus(VectorSource(train_test_set$text))
 train_test_corpus <- tm_map(train_test_corpus, content_transformer(tolower))
 train_test_corpus <- tm_map(train_test_corpus, content_transformer(removePunctuation))
 train_test_corpus <- tm_map(train_test_corpus, content_transformer(removeWords) , c(stopwords('english')))
-train_test_corpus[[1]]
+# train_test_corpus[[1]]
 train_test_corpus <- tm_map(train_test_corpus, content_transformer(stripWhitespace))
-train_test_corpus[[1]]
+# train_test_corpus[[1]]
 
 # Convert to DTM
 DTM_train_test <- DocumentTermMatrix(train_test_corpus,control=list(stemming = TRUE))
@@ -40,24 +41,24 @@ test_df_withclass <- tail (train_test_df_withclass,50)
 
 # dot "." means that we are training using all the features
 # test_df_withclass is the new data that we want to predict the labels
-dt_model <- rpart(classification ~ ., train_df_withclass, method="class")
-prediction <- predict(dt_model, test_df_withclass, type="class")
-table (test_df_withclass$classification, prediction)
+dt_model <- rpart(classification ~ ., train_df_withclass, method="exp")
+prediction <- predict(dt_model, test_df_withclass, type="vector")
+# table (test_df_withclass$classification, prediction)
 
 # Only using the camera and car features to create de model
 dt_model <- rpart(classification ~ camera+car,train_df_withclass,method="class")
 prediction <- predict(dt_model, test_df_withclass, type="class")
-table (test_df_withclass$classification, prediction)
+# table (test_df_withclass$classification, prediction)
 
 # Only use feature "also"; using ANOVA(Analysis of Variance) method 
 dt_model <- rpart(classification ~ also,train_df_withclass,method="anova")
 prediction <- predict(dt_model, test_df_withclass, type="vector")
-table (test_df_withclass$classification, prediction)
+# table (test_df_withclass$classification, prediction)
 
 
-dt_model <- rpart(classification ~ also,train_df_withclass,method="poisson")
+dt_model <- rpart(classification ~ Surv(also),train_df_withclass,method="poisson")
 prediction <- predict(dt_model, test_df_withclass, type="vector")
-table (test_df_withclass$classification, prediction)
+# table (test_df_withclass$classification, prediction)
 
 # Inspecting the dataset
-colnames(test_df_withclass)
+# colnames(test_df_withclass)
